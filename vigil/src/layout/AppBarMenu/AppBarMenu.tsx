7a1e494cedde452861b6acb30eb453f4e0fbb9cf
lindow-ai <remy.lefoll98@gmail.com>
2023-07-05 16:45:38 +0200

    design app bar

--- vigil/src/layout/AppBarMenu/AppBarMenu.tsx
+++ vigil/src/layout/AppBarMenu/AppBarMenu.tsx
@@ -1,43 +1,100 @@
-import { AcmeLogo } from "../../assets/AcmeLogo";
 import "./styles/AppBarMenuStyle.css";
-import { ChevronRight, Home } from "react-iconly";
 
 const AppBarMenu = () => {
   return (
-    <nav className="sidebar">
-      <header>
-        <div className="image-text">
-          <span className="image">
-            <AcmeLogo />
-          </span>
-          <div className="text header-text">
-            <span className="name">Lindow</span>
-            <span className="profession">Web developper</span>
+    <div>
+      <head>
+        <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet' />
+      </head>
+      <nav className="sidebar">
+        <header>
+          <div className="image-text">
+            <span className="image">
+              <img src="logo.png" alt="" />
+            </span>
+
+            <div className="text logo-text">
+              <span className="name">Lindow</span>
+              <span className="profession">Web developer</span>
+            </div>
           </div>
-          <div className="toogle">
-            <ChevronRight set="bold" />
+
+          <i className='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div className="menu-bar">
+          <div className="menu">
+            <ul className="menu-links">
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-home-alt icon' ></i>
+                  <span className="text nav-text">Dashboard</span>
+                </a>
+              </li>
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                  <span className="text nav-text">Revenue</span>
+                </a>
+              </li>
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-bell icon'></i>
+                  <span className="text nav-text">Notifications</span>
+                </a>
+              </li>
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-pie-chart-alt icon' ></i>
+                  <span className="text nav-text">Analytics</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-heart icon' ></i>
+                  <span className="text nav-text">Likes</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className='bx bx-wallet icon' ></i>
+                  <span className="text nav-text">Wallets</span>
+                </a>
+              </li>
+            </ul>
           </div>
-        </div>
-      </header>
-      <div className="menu-bar">
-        <div className="menu">
-          <ul className="menu-link">
-            <li className="nav-link">
+
+          <div className="bottom-content">
+            <li className="">
               <a href="#">
-                <div className="icon"><Home set="bold" /></div>
-                <span>Dashboard</span>
+                <i className='bx bx-log-out icon' ></i>
+                <span className="text nav-text">Logout</span>
               </a>
             </li>
-            <li className="nav-link">
-              <a href="#">
-                <div className="icon"><Home set="bold" /></div>
-                <span>Dashboard</span>
-              </a>
+
+            <li className="mode">
+              <div className="sun-moon">
+                <i className='bx bx-moon icon moon'></i>
+                <i className='bx bx-sun icon sun'></i>
+              </div>
+              <span className="mode-text text">Dark mode</span>
+
+              <div className="toggle-switch">
+                <span className="switch"></span>
+              </div>
             </li>
-          </ul>
+
+          </div>
         </div>
-      </div>
-    </nav>
+
+      </nav>
+
+      <section className="home">
+        <div className="text">Dashboard Sidebar</div>
+      </section>
+    </div>
   );
 };
 
